4564b6029337d9d53997ee419d1ce9474b1e6a92
Fadhiil Rachman <fadhiilrachman@gmail.com>
2022-03-10 06:40:17 +0700

    Release Kopi Academy landing page
    Version 1.0.0

---
+++ precache-manifest.0eb7ecb688b92f610c8e19ecedd13e75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "57886b7d10cd9d177c62",
+    "url": "/static/css/main.ffbc7463.chunk.css"
+  },
+  {
+    "revision": "57886b7d10cd9d177c62",
+    "url": "/static/js/main.57886b7d.chunk.js"
+  },
+  {
+    "revision": "6c1c284ef07abe4d3ce7",
+    "url": "/static/js/1.6c1c284e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6471896055da5823ab76499f8164744f",
+    "url": "/static/media/logo.64718960.svg"
+  },
+  {
+    "revision": "82b2696f465babd57d8265f63d1819a0",
+    "url": "/static/media/kopi-academy.82b2696f.svg"
+  },
+  {
+    "revision": "55d7c01327150004f7d1c39f6d1b486d",
+    "url": "/index.html"
+  }
+];
